40e88880ad168f8d0211a002a6ba96e1cc8695c8
Hayoun Lee <sidhlee@gmail.com>
2020-01-01 17:30:13 +0900

    Add tests

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    counter: 0
+  };
   render() {
     return (
-      <div data-test="component-app ">
+      <div data-test="component-app">
         <h1 data-test="counter-display">The counter is currently</h1>
         <button data-test="increment-button">Increment</button>
       </div>
--- src/App.test.js
+++ src/App.test.js
@@ -23,26 +23,28 @@ const setup = (props = {}, state = null) => {
  * @returns {ShallowWrapper}
  */
 const findByAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test]=${val}`);
+  return wrapper.find(`[data-test="${val}"]`);
 };
 
 test("renders without error", () => {
   // test passes if /index.js renders App without error
   const wrapper = setup();
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const appComponent = findByAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 test("renders increment button", () => {
   const wrapper = setup();
-  const button = wrapper.find("[data-test='increment-button']");
+  const button = findByAttr(wrapper, "increment-button");
   expect(button.length).toBe(1);
 });
 test("renders counter display", () => {
   const wrapper = setup();
-  const counterDisplay = wrapper.find(
-    "[data-test='counter-display']"
-  );
+  const counterDisplay = findByAttr(wrapper, "counter-display");
   expect(counterDisplay.length).toBe(1);
 });
-test("counter starts at 0", () => {});
+test("counter starts at 0", () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toBe(0);
+});
 test("clicking button increments counter display", () => {});
