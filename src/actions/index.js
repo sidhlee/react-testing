19f3689a2099dc02fdf7ee080a74db4f71e8b122
Hayoun Lee <sidhlee@gmail.com>
2020-01-07 18:09:36 +0900

    Cut correctGuess action creator
    It is going to be dispatched inside guessWord

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,3 @@
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS"
 };
-
-/**
- * @function correctGuess
- * @returns {object} - Action object with type `CORRECT_GUESS`
- */
-export const correctGuess = () => ({
-  type: actionTypes.CORRECT_GUESS
-});
