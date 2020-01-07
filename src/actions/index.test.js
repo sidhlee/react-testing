b5ee8f2142df026d14dfd21b6dced2cedfc17131
Hayoun Lee <sidhlee@gmail.com>
2020-01-07 18:27:21 +0900

    Add BP for reducer + combine + Cut unused test file

--- src/actions/index.test.js
+++
@@ -1,9 +0,0 @@
-import { correctGuess, actionTypes } from "./";
-
-describe("correctGuess", () => {
-  test("returns an action with type 'CORRECT_GUESS", () => {
-    const action = correctGuess();
-    // toEqual() compares deep equality (recursively)
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  });
-});
--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,1 +1,11 @@
 // test is done in integration.test.js
+
+/**
+ * @function guessedWordsReducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action - action to be reduced.
+ * @returns {array} - New guessed words state.
+ */
+export default (state, action) => {
+  return null;
+};
