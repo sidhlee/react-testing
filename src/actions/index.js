51b5f48ba2bb0c679bf680e11dc2c6fcc07156c4
Hayoun Lee <sidhlee@gmail.com>
2020-01-09 23:13:01 +0900

    Add secretWord action creator and reducer
    First wrote test with moxios

--- src/actions/index.js
+++ src/actions/index.js
@@ -37,6 +37,11 @@ export const guessWord = guessedWord => {
 
 export const getSecretWord = () => {
   return dispatch => {
-    axios.get("http://localhost:3030");
+    return axios.get("http://localhost:3030").then(res => {
+      dispatch({
+        type: actionTypes.SET_SECRET_WORD,
+        payload: res.data
+      });
+    });
   };
 };
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,5 +1,8 @@
 import moxios from "moxios";
 
+import { storeFactory } from "../../test/testUtils";
+import { getSecretWord } from "./index";
+
 describe("getSecretWord action creator", () => {
   beforeEach(() => {
     moxios.install();
@@ -7,5 +10,21 @@ describe("getSecretWord action creator", () => {
   afterEach(() => {
     moxios.uninstall();
   });
-  test("add response word to state", () => {});
+  test("add response word to state", () => {
+    const secretWord = "party";
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord
+      });
+    });
+    // this function will wait for the returning value to resolve
+    return store.dispatch(getSecretWord()).then(() => {
+      const newState = store.getState();
+      expect(newState.secretWord).toBe(secretWord);
+    });
+  });
 });
