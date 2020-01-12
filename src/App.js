d6848cd8572349abbe5fcfefe47e2bfaad75e46a
Hayoun Lee <sidhlee@gmail.com>
2020-01-12 17:43:22 +0900

    connect App component + nest Input component

--- src/App.js
+++ src/App.js
@@ -2,28 +2,27 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import Congrats from "./Congrats";
+import Input from "./Input";
 import GuessedWords from "./GuessedWords";
+import { getSecretWord } from "./actions";
 
 class App extends Component {
-  state = {
-    guessedWords: [
-      { guessedWord: "train", letterMatchCount: 3 },
-      { guessedWord: "agile", letterMatchCount: 1 },
-      { guessedWord: "party", letterMatchCount: 5 }
-    ]
-  };
-
   render() {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={this.state.guessedWords} />
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-const mapState = state => {};
+const mapState = ({ success, secretWord, guessedWords }) => ({
+  success,
+  secretWord,
+  guessedWords
+});
 
-export default connect(mapState)(App);
+export default connect(mapState, { getSecretWord })(App);
