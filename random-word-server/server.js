e0a77e58fab79c211b1e33e5751664f6e24130b9
Hayoun Lee <sidhlee@gmail.com>
2020-01-13 16:23:52 +0900

    Add req handler to server

--- random-word-server/server.js
+++ random-word-server/server.js
@@ -11,6 +11,22 @@ app.use(
   })
 );
 
+// randomly select a five-letter word from json file
+const fileContent = fs.readFileSync(
+  "./five-letter-words.json",
+  "utf-8"
+);
+const words = JSON.parse(fileContent);
+const { fiveLetterWords } = words;
+const randomIndex = Math.floor(
+  Math.random() * fiveLetterWords.length
+);
+const fiveLetterWord = fiveLetterWords[randomIndex];
+
+app.get("/", (req, res) => {
+  res.send(fiveLetterWord);
+});
+
 app.listen(3030, () =>
   console.log(`random-word-server listening on port 3030`)
 );
