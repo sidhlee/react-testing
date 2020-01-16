ba72017b27c752258855faa0ee71034cb22e6208
Hayoun Lee <sidhlee@gmail.com>
2020-01-16 17:38:31 +0900

    Start refactoring NewWordButton component + test

--- src/NewWordButton.js
+++ src/NewWordButton.js
@@ -1,23 +1,20 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getNewWord } from "./actions/";
+import React from "react";
+import PropTypes from "prop-types";
 
-export class UnconnectedNewWordButton extends Component {
-  render() {
-    const newWordButton = this.props.success ? (
-      <button
-        data-test="new-word-button"
-        className="btn btn-success mb-3"
-        onClick={this.props.getNewWord}
-      >
-        New Word
-      </button>
-    ) : null;
-    return <>{newWordButton}</>;
-  }
-}
+const NewWordButton = props => {
+  const newWordButton = this.props.success ? (
+    <button
+      data-test="new-word-button"
+      className="btn btn-success mb-3"
+      onClick={this.props.getNewWord}
+    >
+      New Word
+    </button>
+  ) : null;
 
-const mapState = ({ success }) => ({ success });
-export default connect(mapState, { getNewWord })(
-  UnconnectedNewWordButton
-);
+  return <>{newWordButton}</>;
+};
+
+NewWordButton.propTypes = {};
+
+export default NewWordButton;
