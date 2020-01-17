1e63ce5af42d429efedd2f8af6ead371b57b78bb
Hayoun Lee <sidhlee@gmail.com>
2020-01-17 16:13:23 +0900

    Add gaveUpReducer
    TODO: refactor state to have 3 different status

--- src/reducers/gaveUpReducer.js
+++ src/reducers/gaveUpReducer.js
@@ -1,5 +1,31 @@
-export default (state, action) => {
+/* 
+NOTE: This is not the optimal way to track whether or not the user has given up.
+With two boolean pieces of state (success, gaveUp), we have four possibilities:
+
+success: false, gaveUp: false // user is playing the game
+success: true, gaveUp: false // user guessed correctly
+success: false, gaveUp: true // user gave up
+success: true, gaveUp: true // impossible case 
+
+To improve, you can have one piece of state 'status'
+with three possibilities: inProgress, victory, gaveUp
+It will be much easier for debugging later.
+*/
+
+import { actionTypes } from "../actions";
+
+/**
+ * @function gaveUp
+ * @param {boolean} state - Whether the user has given up.
+ * @param {object} action - Action to be reduced.
+ * @returns {boolean} - gaveUp state.
+ */
+export default (state = false, action) => {
   switch (action.type) {
+    case actionTypes.GIVE_UP:
+      return true;
+    case actionTypes.RESET_GAME:
+      return false;
     default:
       return state;
   }
