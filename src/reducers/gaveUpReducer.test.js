31c1f758b468331c17bd9a11bd71e320d02bf365
Hayoun Lee <sidhlee@gmail.com>
2020-01-17 15:52:10 +0900

    Add test descriptions + gaveUpReducer bp

---
+++ src/reducers/gaveUpReducer.test.js
@@ -0,0 +1,6 @@
+import { actionTypes } from "../actions/";
+import gaveUpReducer from "../reducers/gaveUpReducer";
+
+test("returns default initial state of 'false' when no action is passed", () => {});
+test("returns state of true upon receiving an action of type 'GIVE_UP'", () => {});
+test("returns state of false upon receiving an action of type 'RESET_GAME'", () => {});
