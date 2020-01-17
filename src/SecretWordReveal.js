407856f475448214e66fa4c1cbef1aa629074a37
Hayoun Lee <sidhlee@gmail.com>
2020-01-17 13:50:41 +0900

    Add SecretWordReveal bp + test descriptions

---
+++ src/SecretWordReveal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SecretWordReveal = props => {
+  return <div></div>;
+};
+
+SecretWordReveal.propTypes = {};
+
+export default SecretWordReveal;
---
+++ src/SecretWordReveal.test.js
@@ -0,0 +1,4 @@
+test("renders without error", () => {});
+test("renders no text when 'display' prop is false", () => {});
+test("renders message containing secret word when 'display' prop is true", () => {});
+test("does not throw warning with expected props", () => {});
