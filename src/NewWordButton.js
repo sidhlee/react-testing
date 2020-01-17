327cc88bbce2f2735bb3a78586de03c5fffeabaa
Hayoun Lee <sidhlee@gmail.com>
2020-01-17 09:13:50 +0900

    Cut + Add new tests for <NewWordButton />

--- src/NewWordButton.js
+++ src/NewWordButton.js
@@ -2,11 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const NewWordButton = props => {
-  const newWordButton = this.props.success ? (
+  const newWordButton = props.success ? (
     <button
       data-test="new-word-button"
       className="btn btn-success mb-3"
-      onClick={this.props.getNewWord}
+      onClick={props.getNewWord}
     >
       New Word
     </button>
--- src/NewWordButton.test.js
+++ src/NewWordButton.test.js
@@ -1,111 +1,29 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { findByTestAttr, storeFactory } from "../test/testUtils";
-import NewWordButton, {
-  UnconnectedNewWordButton
-} from "./NewWordButton";
+import NewWordButton from "./NewWordButton";
 import { getNewWord } from "./actions";
 import moxios from "moxios";
+
+const defaultProps = { success: false };
 /**
  * Factory function to create ShallowWrapper for NewWordButton component.
  * @function setup
  * @param {object} initialState - Initial state for this setup
  * @return {ShallowWrapper}
  */
-const setup = (initialState = {}) => {
-  const store = storeFactory(initialState);
-  return shallow(<NewWordButton store={store} />)
-    .dive()
-    .dive();
+const setup = (props = {}) => {
+  const setupProps = {
+    ...defaultProps,
+    ...props
+  };
+  return shallow(<NewWordButton {...setupProps} />);
 };
 
-test("renders nothing if word is not guessed", () => {
-  const wrapper = setup({ success: false });
-  const newWordButton = findByTestAttr(wrapper, "new-word-button");
-  expect(newWordButton.length).toBe(0);
-});
-
-describe("word has been successfully guessed", () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = setup({ success: true });
-  });
-  test("renders new word button", () => {
-    const newWordButton = findByTestAttr(wrapper, "new-word-button");
-    expect(newWordButton.length).toBe(1);
-  });
-  describe("redux props", () => {
-    test("has a success piece of state as prop", () => {
-      const successProp = wrapper.instance().props.success;
-      expect(successProp).toBe(true);
-    });
-    test("`getNewWord` action creator is a function prop", () => {
-      const getNewWordProp = wrapper.instance().props.getNewWord;
-      expect(getNewWordProp).toBeInstanceOf(Function);
-    });
-  });
-});
-
-describe("`getNewWord` action creator call", () => {
-  let getNewWordMock, wrapper;
-
-  beforeEach(() => {
-    getNewWordMock = jest.fn();
-    wrapper = shallow(
-      <UnconnectedNewWordButton getNewWord={getNewWordMock} success />
-    );
-    const newWordButton = findByTestAttr(wrapper, "new-word-button");
-    newWordButton.simulate("click");
-  });
-  test("`getNewWord` action creator runs on NewWord button click", () => {
-    const getNewWordCallCount = getNewWordMock.mock.calls.length;
-    expect(getNewWordCallCount).toBe(1);
-  });
-
-  describe("`getNewWord` properly updates redux store", () => {
-    let store;
-    const oldSecretWord = "banff";
-    const newSecretWord = "gaudy";
-    const preloadedState = {
-      success: true,
-      guessedWords: [
-        { guessedWord: "fetch", letterMatchCount: 3 },
-        { guessedWord: "ditch", letterMatchCount: 3 },
-        { guessedWord: "bed", letterMatchCount: 3 }
-      ],
-      secretWord: oldSecretWord
-    };
-    beforeEach(() => {
-      // If the action creator that you're testing involves axios call
-      // use moxios to intercept network request and respond with mock data
-      moxios.install();
-      store = storeFactory(preloadedState);
-      moxios.wait(() => {
-        const request = moxios.requests.mostRecent();
-        request.respondWith({
-          status: 200,
-          response: newSecretWord
-        });
-      });
-    });
-    afterEach(() => {
-      moxios.uninstall();
-    });
-    test("clears guessed words list", () => {
-      // When testing thunk with axios call
-      // you MUST RETURN the store.dispatch(thunk) inside test callback
-      // and put expect inside .then()
-      return store.dispatch(getNewWord()).then(() => {
-        const guessedWords = store.getState().guessedWords;
-        expect(guessedWords.length).toBe(0);
-      });
-    });
-    test("gets new secret word", () => {
-      return store.dispatch(getNewWord()).then(() => {
-        const secretWord = store.getState().secretWord;
-        expect(secretWord.length).toBe(5);
-        expect(secretWord).not.toBe("banff");
-      });
-    });
-  });
+describe("render", () => {
+  test("renders without error", () => {});
+  test("renders no text when 'display' prop is false", () => {});
+  test("renders non-empty text when 'display' prop is true", () => {});
+  test("does not throw warning with expected props", () => {});
+  test("calls 'resetAction' prop upon button click", () => {});
 });
