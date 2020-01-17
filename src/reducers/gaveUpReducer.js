8d5072dff19e5c7805d74374f2d09ce64d98b17d
Hayoun Lee <sidhlee@gmail.com>
2020-01-17 16:00:11 +0900

    Fix tests
    Reducer unit test is testing comparing updated state to the previous state. So set state to be different from expected value for effective testing.

--- src/reducers/gaveUpReducer.js
+++ src/reducers/gaveUpReducer.js
@@ -1,4 +1,4 @@
-export default (state = false, action) => {
+export default (state, action) => {
   switch (action.type) {
     default:
       return state;
--- src/reducers/gaveUpReducer.test.js
+++ src/reducers/gaveUpReducer.test.js
@@ -1,6 +1,20 @@
 import { actionTypes } from "../actions/";
 import gaveUpReducer from "../reducers/gaveUpReducer";
 
-test("returns default initial state of 'false' when no action is passed", () => {});
-test("returns state of true upon receiving an action of type 'GIVE_UP'", () => {});
-test("returns state of false upon receiving an action of type 'RESET_GAME'", () => {});
+test("returns default initial state of 'false' when no action is passed", () => {
+  const newState = gaveUpReducer(undefined, {});
+  expect(newState).toBe(false);
+});
+test("returns state of true upon receiving an action of type 'GIVE_UP'", () => {
+  const newState = gaveUpReducer(false, {
+    type: actionTypes.GIVE_UP
+  });
+  expect(newState).toBe(true);
+});
+test("returns state of false upon receiving an action of type 'RESET_GAME'", () => {
+  // start with gaveUp: true, since gaveUp is false by default
+  const newState = gaveUpReducer(true, {
+    type: actionTypes.RESET_GAME
+  });
+  expect(newState).toBe(false);
+});
