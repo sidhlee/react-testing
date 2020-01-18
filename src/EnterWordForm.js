7026cd064fc2ac9048d79217bb865629deb60b96
Hayoun Lee <sidhlee@gmail.com>
2020-01-18 18:22:36 +0900

    Add test descriptions + bp for EnterWordForm

---
+++ src/EnterWordForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export class EnterWordForm extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+EnterWordForm.propTypes = {};
+
+export default EnterWordForm;
---
+++ src/EnterWordForm.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { checkProps, findByTestAttr } from "../test/testUtils";
+import EnterWordForm from "./EnterWordForm";
+
+describe("render", () => {
+  test("renders without error", () => {});
+  test("renders instructions", () => {});
+  test("renders submit button", () => {});
+  test("renders input control", () => {});
+  test("does not throw warning with expected props", () => {});
+});
+
+describe("submit click action", () => {
+  test("'setUserSecretWord' was called once", () => {});
+  test("'setUserSecretWord' was called with input value as argument", () => {});
+});
