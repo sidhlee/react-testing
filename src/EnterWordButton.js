b9b9eac29c8c18b7e6e16f2df209551c25d4e03d
Hayoun Lee <sidhlee@gmail.com>
2020-01-18 16:54:04 +0900

    Start enterWord feature

---
+++ src/EnterWordButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const EnterWordButton = props => {
+  return <div></div>;
+};
+
+EnterWordButton.propTypes = {};
+
+export default EnterWordButton;
---
+++ src/EnterWordButton.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { shallow } from "react";
+
+import { findByTestAttr, checkProps } from "../test/testUtils";
+import EnterWordButton from "./EnterWordButton";
