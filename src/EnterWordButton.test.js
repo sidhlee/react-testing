c23ceca4652120bcba11d2716eccfd97f023b13a
Hayoun Lee <sidhlee@gmail.com>
2020-01-18 17:53:45 +0900

    Add tests for EnterWordButton

--- src/EnterWordButton.test.js
+++ src/EnterWordButton.test.js
@@ -1,13 +1,68 @@
 import React from "react";
-import { shallow } from "react";
+import { shallow } from "enzyme";
 
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import EnterWordButton from "./EnterWordButton";
 
+const defaultProps = { display: true };
+/**
+ * Factory function to create a ShallowWrapper for the EnterWordButton component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  const setupProps = {
+    ...defaultProps,
+    ...props
+  };
+  return shallow(<EnterWordButton {...setupProps} />);
+};
+
 describe("render", () => {
-  test("renders without error", () => {});
-  test("renders no text when 'display' prop is false", () => {});
-  test("does not throw warning with expected props", () => {});
+  test("renders without error", () => {
+    const wrapper = setup();
+    const component = findByTestAttr(
+      wrapper,
+      "component-enter-word-button"
+    );
+    expect(component.length).toBe(1);
+  });
+  test("renders no text when 'display' prop is false", () => {
+    const wrapper = setup({ display: false });
+    const component = findByTestAttr(
+      wrapper,
+      "component-enter-word-button"
+    );
+    expect(component.text()).toBe("");
+  });
+  test("renders non-empty text when 'display' prop is true", () => {
+    const wrapper = setup();
+    const component = findByTestAttr(
+      wrapper,
+      "component-enter-word-button"
+    );
+    expect(component.text().length).not.toBe(0);
+  });
+  test("does not throw warning with expected props", () => {
+    const expectedProps = { display: false };
+    checkProps(EnterWordButton, expectedProps);
+  });
 });
 
-test("calls 'buttonAction' prop upon button click", () => {});
+test("calls 'buttonAction' prop upon button click", () => {
+  const buttonActionMock = jest.fn();
+  const wrapper = setup({
+    display: true,
+    buttonAction: buttonActionMock
+  });
+  const resetButton = findByTestAttr(
+    wrapper,
+    "component-enter-word-button"
+  );
+  // In this case, wrapper and resetButton are the same element
+  console.log("wrapper: ", wrapper.debug()); // rendered element
+  console.log("resetButton: ", resetButton.debug()); // queried element inside rendered element
+  resetButton.simulate("click");
+  expect(buttonActionMock.mock.calls.length).toBe(1);
+});
