cf0bc4e9c4b33acef0adadb78087312544f71419
Hayoun Lee <sidhlee@gmail.com>
2020-01-18 17:32:10 +0900

    Add test descriptions for EnterWordButton

--- src/EnterWordButton.test.js
+++ src/EnterWordButton.test.js
@@ -3,3 +3,11 @@ import { shallow } from "react";
 
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import EnterWordButton from "./EnterWordButton";
+
+describe("render", () => {
+  test("renders without error", () => {});
+  test("renders no text when 'display' prop is false", () => {});
+  test("does not throw warning with expected props", () => {});
+});
+
+test("calls 'buttonAction' prop upon button click", () => {});
