a4cbd50d060b6582bd9d92fddea16a022065464f
Hayoun Lee <sidhlee@gmail.com>
2020-01-18 21:34:37 +0900

    Add userEnterReducer

--- src/reducers/userEnterReducer.js
+++ src/reducers/userEnterReducer.js
@@ -1,5 +1,19 @@
-const userEnterReducer = (state, action) => {
+import { actionTypes } from "../actions/";
+
+/**
+ * @function userEnterReducer
+ * @param {string|null} state - State before reducer.
+ * @param {object} action - Action sent to reducer.
+ * @returns {string|null} - New state (depending on action type).
+ */
+const userEnterReducer = (state = null, action) => {
   switch (action.type) {
+    case actionTypes.USER_ENTERING:
+      return "inProgress";
+    case actionTypes.USER_ENTERED:
+      return "done";
+    case actionTypes.RESET_GAME:
+      return null;
     default:
       return state;
   }
