13797a097dce9b1ff59e4f362246e8beabd5a88a
Hayoun Lee <sidhlee@gmail.com>
2020-01-19 18:31:52 +0900

    Add tests for App state access + render

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { storeFactory } from "../test/testUtils";
+import { storeFactory, findByTestAttr } from "../test/testUtils";
 import App, { UnconnectedApp } from "./App";
 
 const setup = (preloadedState = {}) => {
@@ -18,24 +18,40 @@ describe("Redux props", () => {
   const guessedWords = [
     { guessedWord: "train", letterMatchCount: 3 }
   ];
-  const gaveUp = false;
+  const gaveUp = "don't give up!!";
+  const userEnter = null;
 
   beforeEach(() => {
+    // hydrates default pieces of state
     wrapper = setup({
       success,
       secretWord,
       guessedWords,
-      gaveUp
+      gaveUp,
+      userEnter
     });
   });
+  /* 
+  NOTE: About UnconnectedApp.props
+  - props are passed with mapState. If a prop is not included in mapState,
+  that prop will not be included in UnconnectedApp.props
+  - preloadedState passed to createStore is send to reducers 
+  to replace default state.
+  */
 
   test("has access to `success` state", () => {
+    // wrapper.instance() returns the node's underlying class instance.
+    // i.e. it returns UnconnectedApp
     const successProp = wrapper.instance().props.success;
+    // expects UnconnectedApp to receive success piece of state as prop
+    // which has the value preloaded via 2nd argument to createStore()
     expect(successProp).toBe(success);
   });
   test("has access to 'gaveUp' state", () => {
     const gaveUpProp = wrapper.instance().props.gaveUp;
-    expect(gaveUpProp).toBe(gaveUp);
+    // we can also check that the component receives
+    // expected piece of state as prop regardless of its value.
+    expect(gaveUpProp).not.toBe(undefined);
   });
   test("has access to `secretWord` state", () => {
     const secretWordProp = wrapper.instance().props.secretWord;
@@ -52,6 +68,20 @@ describe("Redux props", () => {
   test("'resetGame' action creator is a function on the props", () => {
     const resetGameProp = wrapper.instance().props.resetGame;
     expect(resetGameProp).toBeInstanceOf(Function);
+  });
+  test("has access to 'userEnter' state", () => {
+    const userEnterProp = wrapper.instance().props.userEnter;
+    expect(userEnterProp).toEqual(userEnter);
+  });
+  test("'setUserEntering' action creator is a function on the props", () => {
+    const setUserEnteringProp = wrapper.instance().props
+      .setUserEntering;
+    expect(setUserEnteringProp).toBeInstanceOf(Function);
+  });
+  test("'setUserSecretWord' action creator is a function on the props", () => {
+    const setUserSecretWordProp = wrapper.instance().props
+      .setUserSecretWord;
+    expect(setUserSecretWordProp).toBeInstanceOf(Function);
   });
 });
 
@@ -74,4 +104,22 @@ test("`getSecretWord` runs on App mount", () => {
   // check to see if the mock ran
   const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
   expect(getSecretWordCallCount).toBe(1);
+});
+
+describe("render", () => {
+  test("renders EnterWordForm when userEnter state is 'inProgress'", () => {
+    const wrapper = setup({ userEnter: "inProgress" });
+    const form = findByTestAttr(wrapper, "component-enter-word-form");
+    expect(form.length).toBe(1);
+  });
+  test("does not render EnterWordForm when userEnter state is 'null'", () => {
+    const wrapper = setup({ userEnter: null });
+    const form = findByTestAttr(wrapper, "component-enter-word-form");
+    expect(form.length).toBe(0);
+  });
+  test("does not render EnterWordForm when userEnter state is 'done'", () => {
+    const wrapper = setup({ userEnter: "done" });
+    const form = findByTestAttr(wrapper, "component-enter-word-form");
+    expect(form.length).toBe(0);
+  });
 });
