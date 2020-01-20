83321469f8455c7800596b05636557c00863f09d
Hayoun Lee <sidhlee@gmail.com>
2020-01-20 17:50:42 +0900

    Add ServerError component

--- src/ServerError.js
+++ src/ServerError.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ServerError = props => {
-  return <div></div>;
+  return (
+    <div data-test="component-server-error">
+      We cannot retrieve the secret word for the moment. Please try
+      again later.
+    </div>
+  );
 };
 
 export default ServerError;
