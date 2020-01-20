8d33f9f4facec5d0834e1e7890e0e708147a49d8
Hayoun Lee <sidhlee@gmail.com>
2020-01-20 18:01:36 +0900

    Add bp + test descriptions for serverErrorReducer

---
+++ src/reducers/serverErrorReducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const serverErrorReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default serverErrorReducer;
---
+++ src/reducers/serverErrorReducer.test.js
@@ -0,0 +1,4 @@
+import serverErrorReducer from "./serverErrorReducer";
+
+test("returns initial state of 'false' when no action is passed", () => {});
+test(" returns state of 'true' upon receiving ac action of type 'SERVER_ERROR'", () => {});
