5cf0599f50710820f6f581e1ff7e1d05934b6323
Hayoun Lee <sidhlee@gmail.com>
2020-01-20 18:09:18 +0900

    Add serverErrorReducer

--- src/reducers/serverErrorReducer.js
+++ src/reducers/serverErrorReducer.js
@@ -1,9 +1,17 @@
 import { actionTypes } from "../actions/";
 
-const initialState = {};
+const initialState = false;
 
+/**
+ * @function serverErrorReducer
+ * @param {boolean} state - State before reducer;
+ * @param {object} action - Action sent to reducer;
+ * @returns {boolean} - New state (depending on action type)
+ */
 const serverErrorReducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.SERVER_ERROR:
+      return true;
     default:
       return state;
   }
