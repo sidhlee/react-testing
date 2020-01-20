26d940084f967c813e1e7434277aa1cfc9e22716
Hayoun Lee <sidhlee@gmail.com>
2020-01-20 19:24:52 +0900

    Add testsfor getSecretWord action creator
    updates serverError state correctly upon 4xx and 5xx response

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,7 +1,7 @@
 import moxios from "moxios";
 
 import { storeFactory } from "../../test/testUtils";
-import { getSecretWord } from "./index";
+import { getSecretWord, getSecretWordDispatch } from "./index";
 
 describe("getSecretWord action creator", () => {
   beforeEach(() => {
@@ -25,6 +25,39 @@ describe("getSecretWord action creator", () => {
     return store.dispatch(getSecretWord()).then(() => {
       const newState = store.getState();
       expect(newState.secretWord).toBe(secretWord);
+    });
+  });
+  describe("updates serverError state to 'true'", () => {
+    // NOTE: there's currently no way to simulate server no-response with moxios
+    test("when server returns 4xx status", () => {
+      const store = storeFactory();
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 404
+        });
+      });
+
+      // updates serverError state to true
+      // when axios responds with 4xx status
+      return store.dispatch(getSecretWord()).then(() => {
+        const newState = store.getState();
+        expect(newState.serverError).toBe(true);
+      });
+    });
+    test("when server returns 5xx status", () => {
+      const store = storeFactory;
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          stats: 500
+        });
+      });
+
+      return store.dispatch(getSecretWord()).then(() => {
+        const newState = store.getState();
+        expect(newState.serverError).toBe(true);
+      });
     });
   });
 });
